9c796f224e26190baecd45b0e0b8b2ebd424a3ba
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2024-07-22 10:02:25 +0200

    No more errors

--- grammar.js
+++ grammar.js
@@ -2,7 +2,7 @@ module.exports = grammar({
   name: 'vbscript',
 
   extras: $ => [
-    /[\t ]/,
+    $._horizontal_whitespace,
     seq('_', /[\n\r]*/),
     $.comment
   ],
@@ -24,15 +24,16 @@ module.exports = grammar({
       prec(2,$.if_statement),
       prec(2,$.for_statement),
       prec(2,$.while_statement),
+      prec(2,$.do_statement),
       prec(2,$.variable_declaration),
       $.invocation_statement,
     ),
 
-    variable_assignment: $ => seq(
+    variable_assignment: $ => prec.left(seq(
       $.identifier,
       $._equal,
       $._expression
-    ),
+    )),
 
     _inline_statement_block: $ => choice(
       repeat1(
@@ -43,8 +44,8 @@ module.exports = grammar({
 
     invocation_statement: $ => prec.left(seq(
       $._right_of_dot_expression,
-      optional(seq($._horizontal_whitespace, $.argument_list)
-    ))),
+      optional($.argument_list)
+    )),
 
     comment: $ => token(seq("'", /.*/)),
 
@@ -84,6 +85,14 @@ module.exports = grammar({
       $._whitespace,
       $._inline_statement_block,
       'Wend'
+    ),
+
+    do_statement: $ => seq(
+      'Do',
+      $._whitespace,
+      $._inline_statement_block,
+      'Loop Until',
+      $._expression
     ),
 
     subroutine: $ => seq(
@@ -187,35 +196,38 @@ module.exports = grammar({
       'Dictionary',
       'Double',
       'Integer',
-      'Integer',
-      'Long',
       'Long',
       'LongLong',
       'LongPtr',
       'Object',
       'Single',
-      'Single',
-      'String',
       'String',
       'Variant'
     ),
 
     _expression: $ => choice(
-      prec(2,$._right_of_dot_expression),
+      prec(2, $.attribute_expression),
       prec(1,seq('(',$._expression, ')')),
       prec(1,$.literal),
       prec(1,$.binary_expression),
       prec(1,$.unary_expression),
+      prec(1,$.function_call),
+      prec(1,$.identifier)
     ),
 
     _right_of_dot_expression: $ => choice(
-      prec(3,$.attribute_expression),
+      prec(3,$._attribute_expression_rest),
       prec(2,$.function_call),
       prec(1,$.identifier)
     ),
 
-    attribute_expression: $ =>
-      prec.left(3,seq($._expression, token.immediate('.'), $._right_of_dot_expression)),
+    attribute_expression: $ => seq(
+      $.identifier,
+      repeat1($._attribute_expression_rest)
+    ),
+
+    _attribute_expression_rest: $ =>
+      prec.left(seq(token.immediate('.'), $._right_of_dot_expression)),
 
     binary_expression: $ => choice(
       prec.left(2,seq($._expression, '+', $._expression)),
